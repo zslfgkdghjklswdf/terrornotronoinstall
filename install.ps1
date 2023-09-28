3958bb6c9fffa840c3d4ca9da9c5f5ed3ce84fea
zslfgkdghjklswdf <120216371+zslfgkdghjklswdf@users.noreply.github.com>
2023-09-28 18:50:25 -0300

    Add files via upload

--- install.ps1
+++ install.ps1
@@ -3,12 +3,12 @@ import-module PowerShellGet
 echo "Iniciando instalao"
 echo "Por estar sendo baixado por powershell, vai demorar bem mais tempo que o normal para baixar"
 echo "E nao feche essa janela porque pode corromper os arquivos do jogo"
-echo " "
-echo " "
-echo " "
-echo " "
-echo " "
-echo " "
+echo "."
+echo "."
+echo "."
+echo "."
+echo "."
+echo "'."
 # Download the Terror no trono.zip file
 Invoke-WebRequest -Uri "https://filetransfer.io/data-package/eeML0ZKe/download" -OutFile "$env:USERPROFILE\Documents\Terror no trono.zip"
 # Extract the Terror no trono.zip file
