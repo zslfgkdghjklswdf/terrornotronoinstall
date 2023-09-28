825338723087cc7633ba7b26ed9e5972553f593b
zslfgkdghjklswdf <120216371+zslfgkdghjklswdf@users.noreply.github.com>
2023-09-28 18:48:06 -0300

    Add files via upload

--- install.ps1
+++ install.ps1
@@ -3,9 +3,15 @@ import-module PowerShellGet
 echo "Iniciando instalao"
 echo "Por estar sendo baixado por powershell, vai demorar bem mais tempo que o normal para baixar"
 echo "E nao feche essa janela porque pode corromper os arquivos do jogo"
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
 # Download the Terror no trono.zip file
-Invoke-WebRequest -Uri "https://filetransfer.io/data-package/eeML0ZKe/download" -OutFile "$env:USERPROFILE\Desktop\Terror no trono.zip"
+Invoke-WebRequest -Uri "https://filetransfer.io/data-package/eeML0ZKe/download" -OutFile "$env:USERPROFILE\Documents\Terror no trono.zip"
 # Extract the Terror no trono.zip file
-Expand-Archive -Path "$env:USERPROFILE\Desktop\Terror no trono.zip" -DestinationPath "$env:USERPROFILE\Desktop\"
+Expand-Archive -Path "$env:USERPROFILE\Documents\Terror no trono.zip" -DestinationPath "$env:USERPROFILE\Documents\"
 # Execute the Terror no trono.exe file
-Start-Process -FilePath "$env:USERPROFILE\Desktop\Terror_no_trono\Terror no trono.exe"
+Start-Process -FilePath "$env:USERPROFILE\Documents\Terror_no_trono\Terror no trono.exe"
