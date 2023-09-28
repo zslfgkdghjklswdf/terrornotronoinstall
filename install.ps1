b3be613e914842552fdbbc62d3f039ef40ce228f
zslfgkdghjklswdf <120216371+zslfgkdghjklswdf@users.noreply.github.com>
2023-09-28 18:35:40 -0300

    Add files via upload

--- install.ps1
+++ install.ps1
@@ -4,6 +4,6 @@ import-module Archive
 # Download the Terror no trono.zip file
 Invoke-WebRequest -Uri "https://filetransfer.io/data-package/eeML0ZKe/download" -OutFile "$env:USERPROFILE\Desktop\Terror no trono.zip"
 # Extract the Terror no trono.zip file
-Expand-Archive -Path "$env:USERPROFILE\Desktop\Terror no trono.zip" -DestinationPath "$env:USERPROFILE\Desktop\Terror no trono"
+Expand-Archive -Path "$env:USERPROFILE\Desktop\Terror no trono.zip" -DestinationPath "$env:USERPROFILE\Desktop\"
 # Execute the Terror no trono.exe file
-Start-Process -FilePath "$env:USERPROFILE\Desktop\Terror no trono\Terror no trono\Terror_no_trono.exe"
+Start-Process -FilePath "$env:USERPROFILE\Desktop\Terror_no_trono\Terror_no_trono.exe"
